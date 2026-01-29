260686f70ef7a0f6633e3a41f3f3d478fd77670a
KJ-infosec <teykj0726@icloud.com>
2026-01-29 23:08:15 +0800

    Level-1

--- src/main/java/KJ.java
+++ src/main/java/KJ.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class KJ {
-    private static String[] tasks = new String[100];
-    private static int count = 0;
     private static final String LINE = "————————————————————————————————————————————————————————";
 
     private static void greeting(){
@@ -20,10 +18,10 @@ public class KJ {
             if(input.equals("bye")) {
                 byeMessage();
                 break;
-            } else if (input.equals("list")) {
-                listMessage();
             } else {
-                handleAdd(input);
+                System.out.println(LINE);
+                System.out.println(input);
+                System.out.println(LINE);
             }
         }
     }
@@ -31,22 +29,6 @@ public class KJ {
     public static void byeMessage() {
         System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
-    }
-
-    public static void listMessage() {
-        System.out.println(LINE);
-        for(int i = 0; i < count ; i++) {
-            System.out.println(" " + (i + 1) + ". " + tasks[i]);
-        }
-        System.out.println(LINE);
-    }
-
-    public static void handleAdd(String input) {
-        tasks[count] = input;
-        count++;
-        System.out.println(LINE);
-        System.out.println("added: " + input);
         System.out.println(LINE);
     }
 

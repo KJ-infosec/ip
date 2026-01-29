f6c7e45c70bd0150a58960b5ac0d0fcbe949299c
KJ-infosec <teykj0726@icloud.com>
2026-01-29 23:10:58 +0800

    Level-0

--- src/main/java/KJ.java
+++ src/main/java/KJ.java
@@ -8,33 +8,14 @@ public class KJ {
      System.out.println(" Hello! I'm KJ");
      System.out.println(" What can I do for you?");
      System.out.println(LINE);
+     System.out.println(" Bye. Hope to see you again soon!");
+     System.out.println(LINE);
     }
 
-    private static void readInput() {
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            String input = scanner.nextLine();
-            if(input.equals("bye")) {
-                byeMessage();
-                break;
-            } else {
-                System.out.println(LINE);
-                System.out.println(input);
-                System.out.println(LINE);
-            }
-        }
-    }
-
-    public static void byeMessage() {
-        System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
-    }
 
     public static void main() {
         greeting();
-        readInput();
     }
 
 }
